43df5a52c8aba41ff4ff4ed660a070947f274c06
zezo0101 <135081596+zezo0101@users.noreply.github.com>
2025-11-08 14:30:54 +0200

    Add files via upload

--- solve.py
+++ solve.py
@@ -1,11 +1,13 @@
-
-
 enc = 'ad82849d96d4b40ef692099f0a57ff62e259d46ea101e89d55d24938f92c7e58910cedf87f1e931d3cd7a1754d952191' #encrypted flag
 encc = [enc[i:i+32] for i in range(0,len(enc),32)] # split the 3 cipher blocks
 
 print(encc)
-#toxic = encc[1][:14*2]+"aa"+encc[1][-1*2:]+encc[2]
-toxic = encc[0][:15*2]+"aa"+encc[1] # first cipher block with last byte is anyhting to bypass the cheating detection
+
+toxic = encc[1][:15*2]+"aa"+encc[2] 
+"""
+try to decrypt the last cipher block with second cipher block before it with last byte is anyhting to bypass the cheating detection
+"""
+
 print(len(toxic))
 
 print(toxic) # try decript this
